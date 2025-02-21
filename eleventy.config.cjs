4056a385091453834bffd1db275ad2a722d03240
axemonkey <hi+github@clivemurray.com>
2025-02-21 10:00:24 +0000

    rename src folder

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,11 +1,11 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('hoodstock.rocks/public');
-	eleventyConfig.addPassthroughCopy({ 'hoodstock.rocks/robots.txt': '/robots.txt' });
+	eleventyConfig.addPassthroughCopy('src/public');
+	eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
 	eleventyConfig.setUseGitIgnore(false);
 	eleventyConfig.setServerOptions({
 		// liveReload: false,
 		watch: [
-			'hoodstock.rocks/public/**/*',
+			'src/public/**/*',
 		],
 		showVersion: true,
 	});
